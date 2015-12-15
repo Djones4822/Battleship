07cf3181c35a4a08804a69682bcc45f1f010ed56
Dave Jones <david.jones4822@gmail.com>
2015-12-14 22:48:21 -0500

    Update computer_attack.py

--- computer_attack.py
+++ computer_attack.py
@@ -2,6 +2,8 @@ from ships import AbstractShip, Submarine, PatrolShip, AircraftCarrier
 from board import Board
 from random import choice
 from string import ascii_uppercase as ASCII_UPPERCASE
+from computer_attack import computer_attack
+from player_attack import player_attack
 
 SHIP_NAMES = {  Submarine.NAME : Submarine,
                 AircraftCarrier.NAME : AircraftCarrier,
