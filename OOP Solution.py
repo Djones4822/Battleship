3b89e38fd6f4000de3e8932e24dbdb3e52a734da
Dave Jones <david.jones4822@gmail.com>
2015-12-14 20:25:42 -0500

    Update OOP Solution.py
    Implemented a smart_attack function into the board. 
    
    Also implemented turns and a win/lose mechanic. Players can also set the number of turns they want to give the computer, while they only have 20 turns themselves if attacking.

--- OOP Solution.py
+++ OOP Solution.py
@@ -8,6 +8,7 @@ class AbstractShip(object):
         self.sunk = False
         self.positions = positions
         self.hit_positions = []
+        self.guess_shot = []
 
 
 class Submarine(AbstractShip):
@@ -29,7 +30,7 @@ class PatrolShip(AbstractShip):
 
 
 class Board(object):
-
+    
     COLS_MAP = {ASCII_UPPERCASE[i] : i for i in range(10)}
     COL_LABEL = '     ' + '   '.join(sorted(COLS_MAP.keys()))
     ALL_POSITIONS = [let + str(i) for let in COLS_MAP.keys() for i in range(1,11)]
@@ -79,10 +80,10 @@ class Board(object):
             positions = [col + str(row+i) for i in range(length)]
         
         print '[DEBUGGING] Generated positions for ship: {}'.format(positions)
+        
         for pos in positions:
             if pos not in Board.ALL_POSITIONS:
                 return None, 1
-        
         
         return positions, 0
         
@@ -105,11 +106,45 @@ class Board(object):
         col = Board.COLS_MAP[position[0]]
         row = int(position[1:]) - 1
         return row, col
+        
+    def smart_attack(self):
+        if self.smart_attack_on:
+            for ship in self.ships:
+                if not ship.sunk and ship.hit_positions:
+                    if len(ship.hit_positions) == 1:
+                        if not ship.guess_shot:
+                            know = ship.hit_positions[0]
+                            tries = []
+                            tries.append(chr(ord(know[0])-1) + know[1:])
+                            tries.append(chr(ord(know[0])+1) + know[1:])
+                            tries.append(know[0] + str(int(know[1:]) + 1))
+                            tries.append(know[0] + str(int(know[1:]) - 1))
+                            print ('DEBUGGING: All Possible tries: {}'.format(tries))
+                            for try_shot in tries:
+                                if try_shot in Board.ALL_POSITIONS and try_shot not in self.shot_positions:
+                                    ship.guess_shot.append(try_shot)
+                        print('DEBUGGING: Valid tries: {}'.format(ship.guess_shot))
+                        shot = choice(ship.guess_shot)
+                        ship.guess_shot.remove(shot)
+                        print ('DEBUGGING: Chosen shot: {} \nDEBUGGING: remaining guesses: {}'.format(shot, ship.guess_shot))
+                        return shot
+                    else:
+                        knowns = ship.hit_positions[-2:]
+                        known1, known2 = knowns[0], knowns[1]
+                        k1_col, k1_row = ord(known1[0]), int(known1[1:])
+                        k2_col, k2_row = ord(known2[0]), int(known2[1:])
+                        slope = (k2_col - k1_col, k2_row - k1_row)
+                        index = (k2_col + slope[0], k2_row + slope[1])
+                        shot = chr(index[0]) + str(index[1])
+                        print ('DEBUGGING: slope: {}\tknown1: {}\tknown2: {}\tshot: {}'.format(slope, known1, known2, shot))
+                        return shot
+        return choice([pos for pos in self.ALL_POSITIONS if pos not in self.shot_positions])
 
-SHIP_NAMES = {  PatrolShip.NAME : PatrolShip,
-                Submarine.NAME : Submarine,
-                AircraftCarrier.NAME : AircraftCarrier
-                }        
+
+SHIP_NAMES = {  Submarine.NAME : Submarine,
+                AircraftCarrier.NAME : AircraftCarrier,
+                PatrolShip.NAME : PatrolShip
+                }
 
 BAD_PLACEMENT_MESSAGE = '''
 You've entered an invalid position. Please check to 
@@ -123,6 +158,26 @@ you want vertical) or the left (if you want horizontal).
 def computer_attack():
     board = Board()
     board.comp_attack = True
+    
+    print 'Would you like to play a smart or a dumb computer?\nEnter 1 for Smart or 2 for Dumb\n'
+    while True:
+        smart_or_dumb = raw_input('-> ')
+        if smart_or_dumb == '1' or smart_or_dumb == '2':
+            board.smart_attack_on = not bool(int(smart_or_dumb)-1)
+            break
+        else:
+            print 'Enter 1 or 2 please.'
+    
+    print 'How many tries do you want to give the computer? Enter any number'
+    response = None
+    while True:
+        try:
+            response = int(raw_input('-> '))
+            break
+        except ValueError:
+            pass
+    comp_tries = response
+    
     
     #settup loop for each ship
     for ship_name in SHIP_NAMES.keys():
@@ -155,9 +210,9 @@ def computer_attack():
                    print BAD_PLACEMENT_MESSAGE
 
     #main play - computer sends consecutive attacks and player is asked if hit      
-    while True:
+    while comp_tries:
         print board
-        shot = choice([pos for pos in board.ALL_POSITIONS if pos not in board.shot_positions])
+        shot = board.smart_attack()
         shot_return, shot_return_ship = board.shoot(shot)
         print 'Computer shoots {}!'.format(shot)
         #ask user about hit
@@ -186,18 +241,24 @@ def computer_attack():
         #End turn, check gameover status
         if all(ship.sunk == True for ship in board.ships):
             print board
-            print 'Game Over! Lets be honest though, it was inevitable. Sorry.'
+            print 'Game Over! Lets be honest though, it was inevitable. You suck.'
             break
         
         #DEBUGGING
         for ship in board.ships:
             print ship.NAME, ship.positions, ship.hit_positions, ship.sunk
-    
+        comp_tries -= 1
+        
+        if not comp_tries:
+            print 'You win!'
+            if board.smart_attack_on and response >= 20:
+                print 'You\'re smarter than our best computer! Good job! You should probably work for the government or something, I donno. What am I, a scientist?\n'
     return None
     
 def player_attack():
     board = Board()
     board.comp_attack = False
+    player_shots = 20
     #set computer positions
     for Ship in SHIP_NAMES.values():
         while True:
@@ -207,9 +268,9 @@ def player_attack():
             if value == 0:
                 board.ships.append(Ship(pos))
                 break
-    print '\nComputer is Ready!'
+    print 'Computer is Ready!\nYou have 20 Shots to win...\n\nGood Luck!'
     
-    while any(ship.sunk == False for ship in board.ships):
+    while any(ship.sunk == False for ship in board.ships) and player_shots:
         print board
         print 'Where would you like to attack?\n'
         user_attack = raw_input('-> ').upper()
@@ -226,8 +287,10 @@ def player_attack():
         else:
             print 'You\'ve either entered and invalid position or you\'ve \
 already shot there!'
-
-    print 'Game over! You sunk all of the ships!'
+    if not player_shots:
+        'You lose. Sorry! Better luck next time!'
+    else:
+        print 'You win!! You sunk all of the ships!'
     return None
         
     
